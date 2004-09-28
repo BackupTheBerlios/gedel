9f1e82b9f5285f169c5acd9caeb078e146eab311
jemore <jemore>
2004-09-28 19:42:09 +0000

    *** empty log message ***

---
+++ gedel_v1/run.bat
@@ -0,0 +1,6 @@
+
+echo Demarrage de l'application...
+java -cp bin/;META-INF/lib/mysql-connector-java-3.0.14-production-bin.jar;META-INF/lib/smartjcommon_1.4.jar;META-INF/lib/smartjprint_1.4.1.jar ecole/EcoleApp
+
+pause
+
