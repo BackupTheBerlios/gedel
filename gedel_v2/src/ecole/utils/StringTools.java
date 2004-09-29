0e8ed43b424cdfdb71b7f3c8bb87b0c8e823cc4c
jemore <jemore>
2004-09-29 15:15:51 +0000

    *** empty log message ***

--- gedel_v2/src/ecole/utils/StringTools.java
+++ gedel_v2/src/ecole/utils/StringTools.java
@@ -5,12 +5,19 @@
 package ecole.utils;
 
 /**
- * TODO : Add javadoc here
+ * Classe utilitaire travaillant sur les chaines de caractères
  * 
  * @author jemore
  */
 public final class StringTools
 {
+    /**
+     * Test si une chaine est vide
+     * @param str
+     * @return true si str == "" ou str == null
+     * @author jerome forestier @ sqli
+     * @date 29 sept. 2004
+     */
 	public static final boolean isEmptyStr(String str)
 	{
 		if (null == str || "".equals(str))
@@ -18,4 +25,19 @@ public final class StringTools
 		else
 			return false;
 	}
+    
+    /**
+     * Retourne une plurielisation de chaine
+     * @param singulier chaine au singulier
+     * @param pluriel chaine au pluriel
+     * @param n nombre 
+     * @return si n > 1, retourne plurieul, sinon singulier
+     * @author jerome forestier @ sqli
+     * @date 29 sept. 2004
+     */
+    public static final String pluriel(String singulier, String pluriel, int n)
+    {
+        if (n > 1) return pluriel;
+        else return singulier;
+    }
 }
